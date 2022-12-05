e4b35dcf485c373cc3a56c22cfd43e13769c3ace
Mohammed Hyder <mohammedhyder@hotmail.com>
2022-12-05 07:40:42 +0200

    الرجول

--- lib/print_pdf.dart
+++ lib/print_pdf.dart
@@ -2,7 +2,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:permission_handler/permission_handler.dart';
